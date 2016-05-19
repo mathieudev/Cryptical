dea74947e1940a3014d27025989b3041c9ccf9a0
mathieudev <devaudmat@gmail.com>
2016-05-19 11:42:41 +0200

    fix bug with IOError

--- main/cryptical.py
+++ main/cryptical.py
@@ -22,6 +22,7 @@ from argparse import RawTextHelpFormatter
 import sys
 import rsa
 import locker
+import os
 
 
 if __name__ == "__main__":
@@ -79,56 +80,63 @@ if __name__ == "__main__":
                         help='use to securely delete the files you want to '
                              'lock')
 
-    # Print help if no args provided
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
+    try:
+        # Print help if no args provided
+        if len(sys.argv) == 1:
+            parser.print_help()
+            sys.exit(1)
 
-    # Returns data from the options specified
-    args = parser.parse_args()
+        # Returns data from the options specified
+        args = parser.parse_args()
 
-    # Call RSA key pair generation if args --gen provided
-    if args.gen is not None:
-        rsa.gen_rsa_keys(int(args.gen))
+        # Call RSA key pair generation if args --gen provided
+        if args.gen is not None:
+            rsa.gen_rsa_keys(int(args.gen))
 
-    # Get RSA key pair if args --keys provided
-    elif args.keys is not None:
-        rsa_private_key = args.keys[0].name
-        print("RSA private key : %s" % rsa_private_key)
-        rsa_public_key = args.keys[1].name
-        print("RSA public key : %s" % rsa_public_key)
+        # Get RSA key pair if args --keys provided
+        elif args.keys is not None:
+            rsa_private_key = args.keys[0].name
+            print("RSA private key : %s" % rsa_private_key)
+            rsa_public_key = args.keys[1].name
+            print("RSA public key : %s" % rsa_public_key)
 
-        # Call locking mechanism if args --lock provided
-        if args.lock is not None:
-            files = []
+            # Call locking mechanism if args --lock provided
+            if args.lock is not None:
+                files = []
 
-            for file in args.lock:
-                files.append(file.name)
-            print("Locking files %s" % files)
+                for file in args.lock:
+                    files.append(file.name)
 
-            # Secure delete sources files if user use --delete
-            if args.delete:
-                secure_delete = True
-            else:
-                secure_delete = False
+                print("Locking files %s" % files)
 
-            # if no name for output, use archive
-            if args.output is not None:
-                output = str(args.output[0])
-            else:
-                output = 'archive'
+                # Secure delete sources files if user use --delete
+                if args.delete:
+                    secure_delete = True
+                else:
+                    secure_delete = False
 
-            locker.lock_files(files, rsa_private_key, rsa_public_key, output,
-                              secure_delete)
-            print("Files locked in %s.lkd" % output)
+                # if no name for output, use archive
+                if args.output is not None:
+                    output = str(args.output[0])
+                else:
+                    output = 'archive'
 
-        # Call unlocking mechanism if args --unlock provided
-        elif args.unlock is not None:
-            print("Unlocking archive %s" % args.unlock.name)
-            locker.unlock_file(args.unlock.name, rsa_private_key,
-                               rsa_public_key)
-    else:
-        print("Provide RSA key pair or generate them")
+                locker.lock_files(files, rsa_private_key, rsa_public_key, output,
+                                  secure_delete)
+                print("Files locked in %s.lkd" % output)
+
+            # Call unlocking mechanism if args --unlock provided
+            elif args.unlock is not None:
+                print("Unlocking archive %s" % args.unlock.name)
+                locker.unlock_file(args.unlock.name, rsa_private_key,
+                                   rsa_public_key)
+        else:
+            print("Provide RSA key pair or generate them.")
+    except IOError as e:
+        print("I/O error({0}): {1}".format(e.errno, e.strerror))
+    except:
+        print("An unexpected error occured.")
+
 
 
 __author__ = 'Hakan Kuesne and Mathieu Devaud'
